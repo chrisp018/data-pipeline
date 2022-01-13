0b82384d7da70e431d9fbf32ffd722da328d45eb
pdkhanh695 <pdkhanh95@gmail.com>
2022-01-14 02:09:30 +0700

    update kafka broker config

--- clickstream-kafka/data-generator/README.md
+++ clickstream-kafka/data-generator/README.md
@@ -1,16 +1,32 @@
+## Test connection
+- Create topic
+```
+sh ./kafka/bin/kafka-topics.sh --bootstrap-server kafka1:9092,kafka2:9092,kafka3:9092 --topic CLICKSTREAM-TEST-CONFIG --create --partitions 3 --replication-factor 2
+```
+- Generate some data
+```
+sh ./kafka/bin/kafka-console-producer.sh --broker-list kafka1:9092,kafka2:9092,kafka3:9092 --topic CLICKSTREAM-TEST-CONFIG
+
+{"device_id": "123", "device_name": "phone"}
+{"device_id": "234", "device_name": "mobile"}
+{"device_id": "345", "device_name": "laptop"}
+```
+- Get data
+```
+sh ./kafka/bin/kafka-console-consumer.sh --bootstrap-server kafka1:9092,kafka2:9092,kafka3:9092 --topic CLICKSTREAM-TEST-CONFIG --from-beginning
+```
+
+## Steps start demo
 1. Create kafka topic 
 ```
-sh ./kafka/bin/kafka-topics.sh --bootstrap-server kafka1:9092 --topic SOURCE_SQL_TOPIC_001 --create --partitions 3 --replication-factor 1
+sh ./kafka/bin/kafka-topics.sh --bootstrap-server kafka1:9092,kafka2:9092,kafka3:9092 --topic SOURCE_SQL_TOPIC_001 --create --partitions 3 --replication-factor 2
 ```
 2. Get list of topics to check whether the topic created or not
 ```
-sh ./kafka/bin/kafka-topics.sh --bootstrap-server kafka1:9092 --list
+sh ./kafka/bin/kafka-topics.sh --bootstrap-server kafka1:9092,kafka2:9092,kafka3:9092 --list
 ```
 3. Start stream data to the kafka
 ```
+cd mgmt
 python3 main.py
-```
-4. Check data from consumer
-```
-sh ./kafka/bin/kafka-console-consumer.sh --bootstrap-server kafka1:9092 --topic SOURCE_SQL_STREAM_001 --from-beginning
 ```
--- clickstream-kafka/ksql/session_transformation.sql
+++ clickstream-kafka/ksql/session_transformation.sql
@@ -2,8 +2,8 @@
 -- Create sources:
 ---------------------------------------------------------------------------------------------------
 -- stream of user clicks:
--- CREATE STREAM source_sql_stream_001 ( user_id BIGINT, device_id VARCHAR, client_event VARCHAR, client_timestamp VARCHAR) WITH ( VALUE_FORMAT='JSON',KAFKA_TOPIC='SOURCE_SQL_TOPIC_001',timestamp='client_timestamp',timestamp_format='yyyy-MM-dd''T''HH:mm:ss.SSSSSS') EMIT CHANGES;
-CREATE STREAM source_sql_stream_001 (
+-- CREATE STREAM SOURCE_SQL_STREAM_001 ( user_id BIGINT, device_id VARCHAR, client_event VARCHAR, client_timestamp VARCHAR) WITH ( VALUE_FORMAT='JSON',KAFKA_TOPIC='SOURCE_SQL_TOPIC_001',timestamp='client_timestamp',timestamp_format='yyyy-MM-dd''T''HH:mm:ss.SSSSSS');
+CREATE STREAM SOURCE_SQL_STREAM_001 (
   user_id BIGINT,
   device_id VARCHAR, 
   client_event VARCHAR, 
@@ -16,15 +16,15 @@ CREATE STREAM source_sql_stream_001 (
     )
   EMIT CHANGES;
 
-DESCRIBE source_sql_stream_001 EXTENDED;
-SELECT user_id, client_event FROM SOURCE_SQL_STREAM_001 EMIT CHANGES;
+DESCRIBE SOURCE_SQL_STREAM_001 EXTENDED;
 SET 'auto.offset.reset'='earliest';
+SELECT user_id, client_event FROM SOURCE_SQL_STREAM_001 EMIT CHANGES;
 
 -- Enrich click-stream with more information:
--- CREATE TABLE enrich_source_sql_stream_001 AS SELECT cast(user_id as VARCHAR) + '_' + SUBSTRING(device_id,1,3) as session_id, TIMESTAMPTOSTRING(WINDOWSTART,'yyyy-MM-dd HH:mm:ss', 'UTC') AS SESSION_START_TS, TIMESTAMPTOSTRING(WINDOWEND,'yyyy-MM-dd HH:mm:ss', 'UTC')   AS SESSION_END_TS, count(*) AS CLICK_COUNT, (WINDOWEND - WINDOWSTART)/1000 AS SESSION_LENGTH_MS FROM SOURCE_SQL_STREAM_001 window SESSION (30 second) GROUP BY cast(user_id as VARCHAR) + '_' + SUBSTRING(device_id,1,3) EMIT CHANGES;
-
-CREATE TABLE enrich_source_sql_stream_001 AS
+-- CREATE TABLE ENRICH_SOURCE_SQL_STREAM_001 AS SELECT cast(user_id as VARCHAR) + ' ' + SUBSTRING(device_id,1,3) as session_id, AS_VALUE(cast(user_id as VARCHAR) + ' ' + SUBSTRING(device_id,1,3)) AS CLICK_SESSION, TIMESTAMPTOSTRING(WINDOWSTART,'yyyy-MM-dd HH:mm:ss', 'UTC') AS SESSION_START_TS, TIMESTAMPTOSTRING(WINDOWEND,'yyyy-MM-dd HH:mm:ss', 'UTC')   AS SESSION_END_TS, count(*) AS CLICK_COUNT, (WINDOWEND - WINDOWSTART)/1000 AS SESSION_LENGTH_MS FROM SOURCE_SQL_STREAM_001 window SESSION (30 second) GROUP BY cast(user_id as VARCHAR) + ' ' + SUBSTRING(device_id,1,3) EMIT CHANGES;
+CREATE TABLE ENRICH_SOURCE_SQL_STREAM_001 AS
   SELECT cast(user_id as VARCHAR) + '_' + SUBSTRING(device_id,1,3) as session_id,
+    AS_VALUE(cast(user_id as VARCHAR) + '_' + SUBSTRING(device_id,1,3)) AS CLICK_SESSION,
     TIMESTAMPTOSTRING(WINDOWSTART,'yyyy-MM-dd HH:mm:ss', 'UTC') AS SESSION_START_TS,
     TIMESTAMPTOSTRING(WINDOWEND,'yyyy-MM-dd HH:mm:ss', 'UTC')   AS SESSION_END_TS,
     count(*) AS CLICK_COUNT,
@@ -32,8 +32,7 @@ CREATE TABLE enrich_source_sql_stream_001 AS
   FROM SOURCE_SQL_STREAM_001 window SESSION (30 second) 
   GROUP BY cast(user_id as VARCHAR) + '_' + SUBSTRING(device_id,1,3) 
   EMIT CHANGES;
--- Create session table with more information:
-CREATE TABLE dest_sql_stream_001 AS (
-  SELECT session_id, session_start_ts, session_end_ts, click_count, session_length_ms 
-  FROM enrich_source_sql_stream_001
-)
+
+DESCRIBE ENRICH_SOURCE_SQL_STREAM_001 EXTENDED;
+SELECT * FROM ENRICH_SOURCE_SQL_STREAM_001 EMIT CHANGES;
+
